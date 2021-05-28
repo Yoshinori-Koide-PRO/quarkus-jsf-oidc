2542ebe44d779ddb026ccf07cccc16113986ccda
PRO小出 <roomtv@pro-japan.co.jp>
2021-05-28 15:35:07 +0900

    OIDCによる認証設定の追加 ログイン情報の取得サンプル

--- showcase/src/main/java/org/apache/myfaces/core/extensions/quarkus/showcase/view/LazyView.java
+++ showcase/src/main/java/org/apache/myfaces/core/extensions/quarkus/showcase/view/LazyView.java
@@ -1,24 +1,21 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.apache.myfaces.core.extensions.quarkus.showcase.view;
 
 import java.io.Serializable;
+import java.security.Principal;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -27,6 +24,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.LazyDataModel;
+import io.quarkus.security.identity.SecurityIdentity;
 
 @Named("dtLazyView")
 @ViewScoped
@@ -40,6 +38,9 @@ public class LazyView implements Serializable {
 
     @Inject
     CarService service;
+
+    @Inject
+    SecurityIdentity identity;
 
     @PostConstruct
     public void init() {
@@ -78,5 +79,9 @@ public class LazyView implements Serializable {
     public void saveInput() {
         System.out.println("Input val is: " + inputVal);
         System.out.println("Input val is null?" + (inputVal == null));
+    }
+
+    public Principal getPrincipal() {
+        return this.identity.getPrincipal();
     }
 }
